0ffb1058d78148bf14a840d693ffb5c169ad7a96
ryryry-3302 <rjleong2002@gmail.com>
2022-05-10 22:50:01 +0800

    stuff

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -1,6 +1,42 @@
+from unicodedata import category
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 class User(AbstractUser):
     pass
+
+class Listing(models.Model):
+    
+    HOME = 'HO'
+    ELECTRONICS = 'EL'
+    SPORTS = 'SO'
+    TOYS = 'TO'
+    FASHION = 'FA'
+
+    types = [
+        (HOME, 'Home'),
+        (ELECTRONICS, 'Electronics'),
+        (SPORTS, 'Sports'),
+        (TOYS, 'Toys'),
+        (FASHION, 'Fashion')
+    ]
+
+
+    title = models.CharField(max_length=30)
+    desc = models.CharField(max_length=400)
+    start_bid =models.DecimalField(max_digits=10,decimal_places=2)
+    imgurl = models.CharField(max_length=200,blank=True)
+    catego = models.CharField(
+        max_length=2,
+        choices=types,
+        blank=True
+        )
+    user = models.ForeignKey(
+        'User',
+        on_delete=models.CASCADE,
+    )
+    
+    
+
+
